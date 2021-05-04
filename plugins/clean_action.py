8196f6b24e933989a80a9b86cef151636760dcf7
buddhhu <buddhhu@ultroid.tech>
2021-05-04 19:15:15 +0000

    pylint: auto fixes

--- plugins/clean_action.py
+++ plugins/clean_action.py
@@ -19,8 +19,10 @@
 
 """
 
-from . import *
 from pyUltroid.functions.clean_db import *
+
+from . import *
+
 
 @ultroid_bot.on(events.ChatAction())
 async def _(event):
@@ -30,31 +32,35 @@ async def _(event):
         except BaseException:
             pass
 
+
 @ultroid_cmd(pattern="addclean$")
 async def _(e):
     if e.chat.admin_rights:
         add_clean(e.chat_id)
         return await eod(e, "Added Clean Action Setting For this Chat")
     return await eod(e, "`ADMIN PERMISSION REQUIRED`")
-  
+
+
 @ultroid_cmd(pattern="remclean$")
 async def _(e):
     if e.chat.admin_rights:
         rem_clean(e.chat_id)
         return await eod(e, "Removed Clean Action Setting For this Chat")
     return await eod(e, "`ADMIN PERMISSION REQUIRED`")
-    
+
+
 @ultroid_cmd(pattern="listclean$")
 async def _(e):
     k = udB.get("CLEANCHAT")
     if k:
-      k = k.split(" ")
-      o = ""
-      for x in k:
-          title = (await ultroid_bot.get_entity(int(x))).title
-          o += x + " " + title + "\n"
-      await eor(e, o)
+        k = k.split(" ")
+        o = ""
+        for x in k:
+            title = (await ultroid_bot.get_entity(int(x))).title
+            o += x + " " + title + "\n"
+        await eor(e, o)
     else:
         await eod(e, "`No Chat Added`")
+
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
