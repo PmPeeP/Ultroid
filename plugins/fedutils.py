c0d101f615d7556d67d62dd40b312872b4cfd99c
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-04 23:12:03 +0530

    Update fedutils.py

--- plugins/fedutils.py
+++ plugins/fedutils.py
@@ -187,7 +187,7 @@ async def _(event):
     except Exception as e:
         print(f"Error in removing FedAdmin file.\n{str(e)}")
     await msg.edit(
-        f"SuperFBan Completed.\nTotal Feds - {len(fedlist)}.\nExcluded - {exCount}.\n Affected {len(fedList) - exCount} feds.\n#TB",
+        f"SuperFBan Completed.\nTotal Feds - {len(fedList)}.\nExcluded - {exCount}.\n Affected {len(fedList) - exCount} feds.\n#TB",
     )
 
 
@@ -335,7 +335,7 @@ async def _(event):
     except Exception as e:
         print(f"Error in removing FedAdmin file.\n{str(e)}")
     await msg.edit(
-        f"SuperUnFBan Completed.\nTotal Feds - {len(fedlist)}.\nExcluded - {exCount}.\n Affected {len(fedList) - exCount} feds.\n#TB",
+        f"SuperUnFBan Completed.\nTotal Feds - {len(fedList)}.\nExcluded - {exCount}.\n Affected {len(fedList) - exCount} feds.\n#TB",
     )
 
 
